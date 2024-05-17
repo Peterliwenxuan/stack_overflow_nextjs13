d7a6ff38922f9662bb55f8b351fd2fd3960ac84b
Peterliwenxuan <leewenxuan2003@outlook.com>
2024-05-17 16:47:50 +0800

    add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,16 +7,44 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import "./globals.css";
+import { Inter, Space_Grotest } from "next/font/google";
+import { Metadata } from "next";
 
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaecGrotest = Inter({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotest",
+});
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description: "DevFlow is a platform for developers to collaborate and learn.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text=primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaecGrotest.variable}`}>
           <header>
             <SignedOut>
               <SignInButton />
